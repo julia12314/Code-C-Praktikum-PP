0630487f75e95dab25d41fbdbd87d7f9bd63b579
Julia <aja823379@gmail.com>
2025-12-19 10:34:00 +0700

    Fix return statement formatting in Pertemuan 2.cpp

---
+++ Pertemuan 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Julia Andini";
+    string NPM = "2517051036";
+
+    cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+    cout << "Nama Saya Adalah " << nama << endl;
+    cout << "NPM Saya Adalah " << NPM << endl << endl;
+
+    cout << "UNILA Indah" << endl;
+    cout << "Be Strong" << endl;
+    return 0;
+}
